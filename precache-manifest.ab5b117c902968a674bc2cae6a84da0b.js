68f440d9f539b3ce9f396bd9d330f32e300ef7f9
Joshuajrodrigues <hitherenewmailhere@gmail.com>
2020-08-28 17:19:40 +0530

    Updates

---
+++ precache-manifest.ab5b117c902968a674bc2cae6a84da0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3bf0cb82156e53e8870691538205435",
+    "url": "/Joshuajrodrigues/movie-search.git/index.html"
+  },
+  {
+    "revision": "d99eecf970218dcae3c2",
+    "url": "/Joshuajrodrigues/movie-search.git/static/css/main.b04d4a5f.chunk.css"
+  },
+  {
+    "revision": "7881d77234d3fa33a1e2",
+    "url": "/Joshuajrodrigues/movie-search.git/static/js/2.eec2883c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Joshuajrodrigues/movie-search.git/static/js/2.eec2883c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d99eecf970218dcae3c2",
+    "url": "/Joshuajrodrigues/movie-search.git/static/js/main.fec1e0a0.chunk.js"
+  },
+  {
+    "revision": "e336fca23b3fafa95ec5",
+    "url": "/Joshuajrodrigues/movie-search.git/static/js/runtime-main.92a7fbbe.js"
+  }
+]);
